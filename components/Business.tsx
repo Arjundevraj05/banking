1d30b37bb119fb2a4ac717de6a2afb449617cf95
Arjun Devraj <arjundevraj05@gmail.com>
2024-09-13 14:49:57 +0530

    uiux2

--- components/Business.tsx
+++ components/Business.tsx
@@ -30,10 +30,10 @@ const Business = () => {
   return (
     <section id="features" className="flex flex-col md:flex-row py-16">
       <div className="flex-1 flex flex-col justify-center">
-        <h2 class="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
-          You do the business, <br class="sm:block hidden" /> we’ll handle the money.
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          You do the business, <br className="sm:block hidden" /> we’ll handle the money.
         </h2>
-        <p class="font-poppins font-normal text-gray-400 text-[18px] leading-[30.8px] ">
+        <p className="font-poppins font-normal text-gray-400 text-[18px] leading-[30.8px] ">
           With the right credit card, you can improve your financial life by building credit, earning rewards, and saving money. But with hundreds of credit cards on the market.
         </p>
 
